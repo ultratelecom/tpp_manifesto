bd379501bb1ba7a43127aabbcc32611cc05260a8
ultratelecom <35201663+ultratelecom@users.noreply.github.com>
2026-01-01 16:27:55 -0400

    Update Urban, Suburban & Village Growth image to THA infrastructure photo

--- next.config.ts
+++ next.config.ts
@@ -15,6 +15,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "i0.wp.com",
       },
+      {
+        protocol: "https",
+        hostname: "www.infrastructuretha.gov.tt",
+      },
     ],
   },
 };
--- src/components/slides/PublicInfrastructureSlide.tsx
+++ src/components/slides/PublicInfrastructureSlide.tsx
@@ -39,7 +39,7 @@ export default function PublicInfrastructureSlide() {
       ],
     },
     {
-      image: "https://images.pexels.com/photos/6635621/pexels-photo-6635621.jpeg?auto=compress&cs=tinysrgb&w=400&h=400&fit=crop",
+      image: "https://www.infrastructuretha.gov.tt/wp-content/uploads/2023/08/011-scaled.jpg",
       title: "Urban, Suburban & Village Growth",
       items: [
         "Multi-Level Parkade in Scarborough",
