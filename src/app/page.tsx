89d8fc648a6cddd4d36c27626d9c6ff7a597d4f3
ultratelecom <35201663+ultratelecom@users.noreply.github.com>
2026-01-01 14:05:57 -0400

    Reorder slides: Constitutional Autonomy, Public Service, Digital Connectivity first; Job Creation section last

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,21 +2,21 @@
 
 import HeroSlide from "@/components/slides/HeroSlide";
 import PillarsSlide from "@/components/slides/PillarsSlide";
+import ConstitutionalAutonomySlide from "@/components/slides/ConstitutionalAutonomySlide";
+import PublicServiceSlide from "@/components/slides/PublicServiceSlide";
+import DigitalConnectivitySlide from "@/components/slides/DigitalConnectivitySlide";
+import EconomicGrowthSlide from "@/components/slides/EconomicGrowthSlide";
+import SustainableTourismSlide from "@/components/slides/SustainableTourismSlide";
+import FoodSecuritySlide from "@/components/slides/FoodSecuritySlide";
+import PublicInfrastructureSlide from "@/components/slides/PublicInfrastructureSlide";
+import EducationSlide from "@/components/slides/EducationSlide";
+import HealthcareSlide from "@/components/slides/HealthcareSlide";
+import AffordableHousingSlide from "@/components/slides/AffordableHousingSlide";
 import DefinitionSlide from "@/components/slides/DefinitionSlide";
 import ImpactSlide from "@/components/slides/ImpactSlide";
 import CleanEnergySlide from "@/components/slides/CleanEnergySlide";
 import DigitalSlide from "@/components/slides/DigitalSlide";
 import InfrastructureSlide from "@/components/slides/InfrastructureSlide";
-import ConstitutionalAutonomySlide from "@/components/slides/ConstitutionalAutonomySlide";
-import EconomicGrowthSlide from "@/components/slides/EconomicGrowthSlide";
-import SustainableTourismSlide from "@/components/slides/SustainableTourismSlide";
-import FoodSecuritySlide from "@/components/slides/FoodSecuritySlide";
-import PublicInfrastructureSlide from "@/components/slides/PublicInfrastructureSlide";
-import PublicServiceSlide from "@/components/slides/PublicServiceSlide";
-import DigitalConnectivitySlide from "@/components/slides/DigitalConnectivitySlide";
-import EducationSlide from "@/components/slides/EducationSlide";
-import HealthcareSlide from "@/components/slides/HealthcareSlide";
-import AffordableHousingSlide from "@/components/slides/AffordableHousingSlide";
 import ClosingSlide from "@/components/slides/ClosingSlide";
 import SideNavigation from "@/components/ui/SideNavigation";
 import ScrollProgress from "@/components/ui/ScrollProgress";
@@ -24,21 +24,21 @@ import ScrollProgress from "@/components/ui/ScrollProgress";
 const slideIds = [
   "hero",
   "pillars",
+  "autonomy",
+  "public-service",
+  "digital-connectivity",
+  "economic-growth",
+  "tourism",
+  "food-security",
+  "public-infrastructure",
+  "education",
+  "healthcare",
+  "housing",
   "definition",
   "impact",
   "clean-energy",
   "digital",
   "infrastructure",
-  "autonomy",
-  "economic-growth",
-  "tourism",
-  "food-security",
-  "public-infrastructure",
-  "public-service",
-  "digital-connectivity",
-  "education",
-  "healthcare",
-  "housing",
   "closing",
 ];
 
@@ -55,24 +55,24 @@ export default function Home() {
       <HeroSlide />
       <PillarsSlide />
 
-      {/* Slides - Sustainable Job Creation Section */}
+      {/* Policy Focus Areas - Priority Order */}
+      <ConstitutionalAutonomySlide />
+      <PublicServiceSlide />
+      <DigitalConnectivitySlide />
+      <EconomicGrowthSlide />
+      <SustainableTourismSlide />
+      <FoodSecuritySlide />
+      <PublicInfrastructureSlide />
+      <EducationSlide />
+      <HealthcareSlide />
+      <AffordableHousingSlide />
+
+      {/* Sustainable Job Creation Section */}
       <DefinitionSlide />
       <ImpactSlide />
       <CleanEnergySlide />
       <DigitalSlide />
       <InfrastructureSlide />
-
-      {/* Slides - Policy Focus Areas */}
-      <ConstitutionalAutonomySlide />
-      <EconomicGrowthSlide />
-      <SustainableTourismSlide />
-      <FoodSecuritySlide />
-      <PublicInfrastructureSlide />
-      <PublicServiceSlide />
-      <DigitalConnectivitySlide />
-      <EducationSlide />
-      <HealthcareSlide />
-      <AffordableHousingSlide />
 
       {/* Closing */}
       <ClosingSlide />
